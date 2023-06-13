fff9c3f4ea476a43d071edf6eee765fda04f456e
Greg Joy <gregjoy91@gmail.com>
2023-06-13 20:28:16 +0100

    Wired up redis cli to crude event ical parser

--- src/lib.rs
+++ src/lib.rs
@@ -10,42 +10,40 @@ extern crate redis_module;
 pub const MODULE_NAME:    &str = "RediCal";
 pub const MODULE_VERSION: u32 = 1;
 
-fn args_test(_: &Context, args: Vec<RedisString>) -> RedisResult {
+fn args_test<'a>(_: &Context, args: Vec<RedisString>) -> RedisResult {
     let response = args.into_iter().map(|arg| RedisValue::SimpleString(arg.to_string())).collect();
 
     Ok(RedisValue::Array(response))
 }
 
-/*
 fn event_set(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
     if args.len() < 2 {
+        ctx.log_debug(format!("event_set WrongArity: {{args.len()}}").as_str());
+
         return Err(RedisError::WrongArity);
     }
 
     let mut args = args.into_iter().skip(1);
 
     let key_name = args.next_arg()?;
-    let other = args.next_arg()?;
 
     let key = ctx.open_key_writable(&key_name);
 
+    let other: String = args.map(|arg| arg.try_as_str().unwrap_or("")).collect::<Vec<&str>>().join(" ").as_str().to_owned();
+
     ctx.log_debug(format!("key: {key_name}, other: {other}").as_str());
 
-    if let Some(event) = key.get_value::<Event>(&EVENT_DATA_TYPE)? {
-        event.other = other.to_string();
-        ctx.log_debug(format!("event: {:?}", event).as_str());
-    } else {
-        let event = Event {
-            uuid:  key_name.to_string(),
-            other: other.to_string()
-        };
+    match Event::parse_ical(other.as_str()) {
+        Ok(event) => {
+            key.set_value(&EVENT_DATA_TYPE, event)?;
 
-        key.set_value(&EVENT_DATA_TYPE, event)?;
+            Ok(other.into())
+        }
+        Err(error) => {
+            Err(RedisError::String(error))
+        }
     }
-
-    Ok(other.into())
 }
-*/
 
 fn event_get(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
     if args.len() < 1 {
@@ -76,7 +74,7 @@ redis_module! {
     ],
     commands:   [
         ["args.test", args_test, "", 0, 0, 0],
-        // ["event.set", event_set, "", 0, 0, 0],
+        ["event.set", event_set, "", 0, 0, 0],
         ["event.get", event_get, "", 0, 0, 0],
     ],
 }
