7465db7ca0ed5f054e103f9de5f062c7e7d82681
Greg Joy <gregjoy91@gmail.com>
2024-06-21 21:35:52 +0100

    Updated rdcl.cal_query command to be blocking

--- redical_redis/src/commands/rdcl_cal_query.rs
+++ redical_redis/src/commands/rdcl_cal_query.rs
@@ -1,4 +1,4 @@
-use redis_module::{Context, NextArg, RedisError, RedisResult, RedisString, RedisValue};
+use redis_module::{Context, NextArg, RedisError, RedisResult, RedisString, RedisValue, ThreadSafeContext};
 
 use std::str::FromStr;
 
@@ -8,6 +8,16 @@ use crate::utils::{run_with_timeout, TimeoutError};
 use crate::CONFIGURATION_ICAL_PARSER_TIMEOUT_MS;
 use redical_core::Calendar;
 use crate::datatype::CALENDAR_DATA_TYPE;
+
+fn icalendar_component_to_redis_value_array<I: ICalendarComponent>(component: &I, rendering_context: &RenderingContext) -> RedisValue {
+    RedisValue::Array(
+        component
+            .to_rendered_content_lines_with_context(Some(&rendering_context))
+            .iter()
+            .map(|ical_part| RedisValue::SimpleString(ical_part.to_owned()))
+            .collect()
+    )
+}
 
 pub fn redical_calendar_query(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
     if args.len() < 2 {
@@ -22,7 +32,7 @@ pub fn redical_calendar_query(ctx: &Context, args: Vec<RedisString>) -> RedisRes
 
     let calendar_key = ctx.open_key(&calendar_uid);
 
-    let Some(calendar) = calendar_key.get_value::<Calendar>(&CALENDAR_DATA_TYPE)? else {
+    let Some(calendar) = calendar_key.get_value::<Calendar>(&CALENDAR_DATA_TYPE)?.cloned() else {
         return Err(RedisError::String(format!(
             "rdcl.cal_query: No Calendar found on key: {calendar_uid}"
         )));
@@ -43,71 +53,86 @@ pub fn redical_calendar_query(ctx: &Context, args: Vec<RedisString>) -> RedisRes
         .as_str()
         .to_owned();
 
-    // Spawn the process of parsing the query into it's own timeout enforced thread to guard
-    // against malicious payloads intended to cause hangs.
-    let mut parsed_query =
-        match run_with_timeout(
-            move || Query::from_str(query_string.as_str()).map_err(RedisError::String),
-            std::time::Duration::from_millis(*CONFIGURATION_ICAL_PARSER_TIMEOUT_MS.lock(ctx) as u64),
-        ) {
-            Ok(parser_result) => {
-                parser_result?
-            },
+    let ical_parser_timeout_ms = *CONFIGURATION_ICAL_PARSER_TIMEOUT_MS.lock(ctx) as u64;
 
-            Err(TimeoutError) => {
-                ctx.log_warning(
-                    format!(
-                        "rdcl.cal_query: query iCal parser exceeded timeout -- calendar_uid: {calendar_uid}",
-                    ).as_str()
-                );
+    let calendar_uid = calendar_uid.to_string();
 
-                return Err(RedisError::String(format!(
-                    "rdcl.cal_query: query iCal parser exceeded timeout"
-                )));
+    let blocked_client = ctx.block_client();
+
+    std::thread::spawn(move || {
+        let thread_ctx = ThreadSafeContext::with_blocked_client(blocked_client);
+
+        // Spawn the process of parsing the query into it's own timeout enforced thread to guard
+        // against malicious payloads intended to cause hangs.
+        let mut parsed_query =
+            match run_with_timeout(
+                move || Query::from_str(query_string.as_str()).map_err(RedisError::String),
+                std::time::Duration::from_millis(ical_parser_timeout_ms),
+            ) {
+                Ok(parser_result) => {
+                    match parser_result {
+                        Ok(parser_result) => parser_result,
+
+                        Err(parser_error) => {
+                            thread_ctx.reply(Err(parser_error));
+
+                            return;
+                        }
+                    }
+                },
+
+                Err(TimeoutError) => {
+                    thread_ctx.lock().log_warning(
+                        format!(
+                            "rdcl.cal_query: query iCal parser exceeded timeout -- calendar_uid: {calendar_uid}",
+                        ).as_str()
+                    );
+
+                    thread_ctx.reply(Err(RedisError::String(String::from("rdcl.cal_query: query iCal parser exceeded timeout"))));
+
+                    return;
+                },
+            };
+
+        thread_ctx.lock().log_debug(
+            format!(
+                "rdcl.cal_query: calendar_uid: {calendar_uid} parsed query: {:#?}",
+                parsed_query
+            ).as_str(),
+        );
+
+        let query_results = match parsed_query.execute(&calendar) {
+            Ok(results) => results,
+
+            Err(error) => {
+                thread_ctx.reply(Err(RedisError::String(error)));
+
+                return;
             },
         };
 
-    ctx.log_debug(
-        format!(
-            "rdcl.cal_query: calendar_uid: {calendar_uid} parsed query: {:#?}",
-            parsed_query
-        ).as_str(),
-    );
+        // TODO: Clean up and properly serialize this griminess
+        let query_result_items = query_results
+            .results
+            .iter()
+            .map(|query_result| {
+                let rendering_context = RenderingContext {
+                    tz: Some(parsed_query.in_timezone.to_owned()),
+                    distance_unit: None,
+                };
 
-    let query_results = parsed_query
-        .execute(calendar)
-        .map_err(|error| RedisError::String(error))?;
+                RedisValue::Array(vec![
+                    icalendar_component_to_redis_value_array(&query_result.result_ordering, &rendering_context),
+                    icalendar_component_to_redis_value_array(&query_result.event_instance, &rendering_context),
+                ])
+            })
+            .collect();
 
-    // TODO: Clean up and properly serialize this griminess
-    let query_result_items = query_results
-        .results
-        .iter()
-        .map(|query_result| {
-            let rendering_context = RenderingContext {
-                tz: Some(parsed_query.in_timezone.to_owned()),
-                distance_unit: None,
-            };
+        thread_ctx.reply(
+            Ok(RedisValue::Array(query_result_items))
+        );
+    });
 
-            RedisValue::Array(vec![
-                RedisValue::Array(
-                    query_result
-                        .result_ordering
-                        .to_rendered_content_lines_with_context(Some(&rendering_context))
-                        .iter()
-                        .map(|ical_part| RedisValue::SimpleString(ical_part.to_owned()))
-                        .collect(),
-                ),
-                RedisValue::Array(
-                    query_result
-                        .event_instance
-                        .to_rendered_content_lines_with_context(Some(&rendering_context))
-                        .iter()
-                        .map(|ical_part| RedisValue::SimpleString(ical_part.to_owned()))
-                        .collect(),
-                ),
-            ])
-        })
-        .collect();
-
-    Ok(RedisValue::Array(query_result_items))
+    // We will reply later, from the thread
+    Ok(RedisValue::NoReply)
 }
